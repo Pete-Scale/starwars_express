59ef0210be1195fc65caa5e068eb687237b7fb02
Pete-Scale <petescale@gmail.com>
2021-01-23 11:09:38 -0600

    added /api/characters route

--- server.js
+++ server.js
@@ -2,9 +2,44 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const characters = [
+    {
+        name: "Yoda",
+        role: "Jedi Master",
+        forcePoints: 100000,
+        age: 900,
+        avatar: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/baby-yoda-old-yoda-1574103229.jpg?crop=0.486xw:0.973xh;0.514xw,0&resize=480:*",
+        routeName: "yoda"
+    },
+    {
+        name: "Luke Skywalker",
+        role: "Jedi Master",
+        forcePoints: 100000,
+        age: 22,
+        avatar: "https://media.contentapi.ea.com/content/dam/star-wars-battlefront-2/images/2019/08/swbf2-refresh-hero-large-heroes-page-luke-skywalker-16x9-xl.jpg.adapt.crop1x1.320w.jpg",
+        routeName: "lukeskywalker"
+    },
+    {
+        name: "Princess Leia",
+        role: "Princess of Alderaan",
+        forcePoints: 100,
+        age: 22,
+        avatar: "https://upload.wikimedia.org/wikipedia/en/1/1b/Princess_Leia%27s_characteristic_hairstyle.jpg",
+        routeName: "yoda"
+    }
+];
+
 app.get("/", (req, res) => {
-    res.send("May the force be with you!")
+    res.send("May the force be with you!");
 });
+
+// /api/characters - show all character data
+app.get("/api/characters", (req, res) => {
+    res.json(characters);
+});
+
+
+// /api/characters/:routeName
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
